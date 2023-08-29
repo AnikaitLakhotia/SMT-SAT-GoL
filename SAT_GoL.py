4676132290a665f8e45b39348514790051487f16
AnikaitLakhotia <117095969+AnikaitLakhotia@users.noreply.github.com>
2023-08-28 22:15:15 -0600

    Input to solver will now be in CNF due to Tseitin's encoding applied on input formula. Also performed some code optimisation and added details on how to test the encoding in a multi-line string towards the end.

--- SAT_GoL.py
+++ SAT_GoL.py
@@ -16,10 +16,17 @@ if __name__ == "__main__":
 
     # Create a Z3 solver
     solver = Solver()
-    reverse_gol_expr = reverse_gol([c1_1_2,c1_2_2,c2_1_2,c2_2_2], {'c1_2': True,'c2_2':True, 'c1_1': True,'c2_1': False},
-                                     range(0, 3), 2, 2, 2)
+
+    reverse_gol_expr = reverse_gol([c1_1_2,c1_2_2,c2_1_2,c2_2_2],
+                                   {'c1_2': True,'c2_2':True, 'c1_1': True,'c2_1': False},2, 2, 2)
+    formula = And(eval(reverse_gol_expr) == True)
+
+    # Apply Tactics
+    tseitin_cnf_tactic = Tactic('tseitin-cnf')
+    cnf_formula = tseitin_cnf_tactic(formula)
+
     # Add constraints to the solver
-    solver.add(And(eval(reverse_gol_expr) == True))
+    solver.add(formula)
 
     # Measure solving time(more accurate than solver statistics) and check satisfiability
     start_time = time.time()
@@ -32,8 +39,21 @@ if __name__ == "__main__":
         print("Satisfiable!")
     else:
         print("Unsatisfiable!")
+
+    #Elapsed Time
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
 
     #Solver Statistics
     print(solver.statistics())
+
+
+    '''Steps to test:
+    Step 1: Declare boolean variables.(Note: The function generate_combinations could be useful)
+    Step 2: Set initial values of input variables(input board values and values for other boards' cells(optional))
+    Step 3: In the definition of reverse_gol_expr, list all the variables of the input board in an array as first
+            argument, constraints on output board(as a dict) as second argument, followed by dimensions of input and 
+            output matrices(both must have same dimensions and entered with dimension of rows followed by that of 
+            columns(both as Int)) and maximum number of allowed generations(Int) assuming 0 is the lowest possible 
+            genration in the respective order
+    Step 4: Run.'''
